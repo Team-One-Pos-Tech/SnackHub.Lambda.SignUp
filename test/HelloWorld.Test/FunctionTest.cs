59edd56841c4bf9388af80f39c0e7fc20383ff12
Kássio San <kciosantana@gmail.com>
2024-08-25 17:12:26 -0300

    TestCreateUserFunctionHandler (RED)

--- test/HelloWorld.Test/FunctionTest.cs
+++ test/HelloWorld.Test/FunctionTest.cs
@@ -14,45 +14,31 @@ namespace HelloWorld.Tests
   {
     private static readonly HttpClient client = new HttpClient();
 
-    private static async Task<string> GetCallingIP()
-    {
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Add("User-Agent", "AWS Lambda .Net Client");
-
-            var stringTask = client.GetStringAsync("http://checkip.amazonaws.com/").ConfigureAwait(continueOnCapturedContext:false);
-
-            var msg = await stringTask;
-            return msg.Replace("\n","");
-    }
-
     [Fact]
-    public async Task TestHelloWorldFunctionHandler()
+    public async Task TestCreateUserFunctionHandler()
     {
-            var request = new APIGatewayProxyRequest();
-            var context = new TestLambdaContext();
-            string location = GetCallingIP().Result;
-            Dictionary<string, string> body = new Dictionary<string, string>
-            {
-                { "message", "hello world" },
-                { "location", location },
-            };
-
-            var expectedResponse = new APIGatewayProxyResponse
-            {
-                Body = JsonSerializer.Serialize(body),
-                StatusCode = 200,
-                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
-            };
-
-            var function = new Function();
-            var response = await function.FunctionHandler(request, context);
-
-            Console.WriteLine("Lambda Response: \n" + response.Body);
-            Console.WriteLine("Expected Response: \n" + expectedResponse.Body);
-
-            Assert.Equal(expectedResponse.Body, response.Body);
-            Assert.Equal(expectedResponse.Headers, response.Headers);
-            Assert.Equal(expectedResponse.StatusCode, response.StatusCode);
+        // Arrange
+        var request = new APIGatewayProxyRequest();
+        var context = new TestLambdaContext();
+        request.Body = @"
+        {
+            ""Cpf"": ""53469738009"",
+            ""Name"": ""junior"",
+        }";
+            
+        // Act
+        var function = new Function();
+        var response = await function.FunctionHandler(request, context);
+        
+        // Assert
+        var expectedResponse = new APIGatewayProxyResponse
+        {
+            StatusCode = 200,
+            Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+        };
+            
+        Assert.Equal(expectedResponse.Headers, response.Headers);
+        Assert.Equal(expectedResponse.StatusCode, response.StatusCode);
     }
   }
 }
